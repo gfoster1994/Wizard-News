c1a600f985cb7211fa9136405306d1a5db20387c
Grant <grant.mc.foster@gmail.com>
2022-10-25 09:55:42 -0500

    added morgan and volleyball middleware, made postBank file with posts data and list/find functions for data

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const volleyball = require('volleyball');
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+//middleware
+app.use(morgan('dev'));
+app.use('volleyball');
 
 const PORT = 1337;
 
