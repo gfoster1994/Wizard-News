6b6f17030d7d6799b78a8f9e6ed41124e0799afd
rayhankhan <khanrayhanali@gmail.com>
2022-10-25 10:41:05 -0500

    Posts and HTML consts + posts mapping

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan');
 const volleyball = require('volleyball');
+const postBank = require("./postBank");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+
+const posts = postBank.list();
+
+const html = `<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Wizard News</title>
+    </head>
+    <body>
+      <ul>
+        ${posts.map(post => `<li>${post.id} ${post.title} ${post.name}</li>`)}
+      </ul>
+    </body>
+  </html>`
+
+  res.send(html);
+});
 
 //middleware
 app.use(morgan('dev'));
-app.use('volleyball');
+app.use(volleyball);
 
 const PORT = 1337;
 
