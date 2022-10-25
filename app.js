c8b4bea8ee7752c785dd118dcf0b06773416bfc7
Grant <grant.mc.foster@gmail.com>
2022-10-25 10:58:23 -0500

    added app.user for static routing on public folder, logo is showing at /logo.png

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const morgan = require('morgan');
 const volleyball = require('volleyball');
 const postBank = require("./postBank");
 
+//middleware
+app.use(morgan('dev'));
+app.use(volleyball);
+app.use(express.static('public'))
+
+app.get("/style.css")
+
 app.get("/", (req, res) => {
 
 const posts = postBank.list();
@@ -15,7 +22,7 @@ const html = `<!DOCTYPE html>
     </head>
     <body>
       <ul>
-        ${posts.map(post => `<li>${post.id} ${post.title} ${post.name}</li>`)}
+        ${posts.map(post => `<li>${post.title} ${post.name}</li>`)}
       </ul>
     </body>
   </html>`
@@ -23,12 +30,7 @@ const html = `<!DOCTYPE html>
   res.send(html);
 });
 
-//middleware
-app.use(morgan('dev'));
-app.use(volleyball);
-
 const PORT = 1337;
-
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
